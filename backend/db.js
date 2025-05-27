f1b34c43476087f671c0024ee1f8473408ded2f9
Mason Hart <mhart5892@gmail.com>
2025-05-26 21:35:39 -0500

    Delete backend/db.js

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require("mysql2/promise");
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",      // ← your MySQL root password
-  database: "fitness_friend"
-});
-module.exports = db;
